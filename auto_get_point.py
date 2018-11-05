ce34d3d4bbe7fe80fc114f6417a73e50f0c438e2
Llona <liu.lon@gmail.com>
2018-11-05 16:22:25 +0800

    renew cookie implement auto login by cookies done

--- auto_get_point.py
+++ auto_get_point.py
@@ -7,6 +7,8 @@ import pyautogui
 import json
 import sys
 
+url = r'https://shopee.tw/'
+coin_url = r'https://shopee.tw/shopee-coins'
 username = ''
 password = ''
 
@@ -26,7 +28,23 @@ def start(options):
 
     if options == 1:
         load_cookies(driver)
-        driver.get(r'https://shopee.tw/')
+        time.sleep(10)
+        driver.get(coin_url)
+        time.sleep(10)
+        print(driver.page_source)
+        # driver.refresh()
+        # time.sleep(30)
+        # click get coin point button
+        # __layout > div > section > div > div.top > div.check-box > div.check-action > div:nth-child(2) > div
+        # element = driver.find_element_by_xpath("//div[@id='__layout']/div/section/div/div[1]/div[1]/div[3]/div[2]/div")
+        element = driver.find_element_by_class_name("check-action")
+        # element = driver.find_element_by_css_selector("div.login-check-btn")
+        # element = driver.find_element_by_tag_name("div data-v-3715bb7c")
+        print(element)
+
+        element.click()
+
+        time.sleep(30)
         # login(driver)
     elif options == 2:
         login(driver)
@@ -34,23 +52,23 @@ def start(options):
         print('save cookies done')
 
 
-def login(driver):
+def login(drivers):
     # login to Shopee
-    driver.get(r'https://shopee.tw/')
+    drivers.get(url)
 
-    time.sleep(2)
+    time.sleep(5)
     # send esc key for skip AD
-    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+    webdriver.ActionChains(drivers).send_keys(Keys.ESCAPE).perform()
     # element = driver.find_element_by_class_name('navbar__link--account')
     time.sleep(2)
     # click login button
-    element = driver.find_element_by_xpath("//div[@id='main']/div/div[2]/div[1]/div/div[1]/div/ul/li[5]")
+    element = drivers.find_element_by_xpath("//div[@id='main']/div/div[2]/div[1]/div/div[1]/div/ul/li[5]")
     element.click()
 
     # press TAB key for into user name field
     tab_num = 0
     while tab_num < 14:
-        webdriver.ActionChains(driver).send_keys(Keys.TAB).perform()
+        webdriver.ActionChains(drivers).send_keys(Keys.TAB).perform()
         tab_num += 1
         time.sleep(0.2)
 
@@ -59,37 +77,34 @@ def login(driver):
     pyautogui.typewrite(password)
 
 
-def renew_shopee_cookies_file(driver):
-    print("After 30 sec. will store cookie to cookie")
-    time.sleep(30)
-    driver.refresh()
+def renew_shopee_cookies_file(drivers):
+    drivers.delete_all_cookies()
+    ss = input("please press enter for get cookie file when browser ready")
+    print(ss)
+    drivers.refresh()
     time.sleep(5)
 
-    cookies = driver.get_cookies()
+    cookies = drivers.get_cookies()
     json_cookies = json.dumps(cookies)
     with open('cookies.json', 'w') as f:
         f.write(json_cookies)
 
 
-def load_cookies(driver):
-    # driver.delete_all_cookies()
-    driver.get(r'https://shopee.tw/')
+def load_cookies(drivers):
+    # login to Shopee first for load cookies
+    drivers.delete_all_cookies()
+    drivers.get(url)
     time.sleep(2)
-    # send esc key for skip AD
-    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
     with open('cookies.json', 'r', encoding='utf-8') as f:
         list_cookies = json.loads(f.read())
 
-    count = 0
     for cookie in list_cookies:
-        count += 1
-        print(count)
-        if cookie['expiry']:
+        if 'expiry' in cookie:
             expiry = cookie['expiry']
         else:
             expiry = None
-        driver.add_cookie({
+        drivers.add_cookie({
             'domain': cookie['domain'],
             'httpOnly': cookie['httpOnly'],
             'secure': cookie['secure'],
@@ -99,10 +114,10 @@ def load_cookies(driver):
             'expiry': expiry
             # 'expires': cookie['expiry']
         })
-    print(list_cookies)
+    # print(list_cookies)
+
 
 if __name__ == '__main__':
-
     if len(sys.argv) >= 2:
         print('into 2')
         if sys.argv[1] == 'cookie':
@@ -113,4 +128,4 @@ if __name__ == '__main__':
         print('into 1')
         start(1)
 
-    driver.close()
+    # driver.close()
